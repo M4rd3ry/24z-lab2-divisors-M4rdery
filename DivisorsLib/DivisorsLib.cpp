5ddf62fde0dc472c6f765498a5a00a6b98cfd0a1
M4rd3ry <mardery77@gmail.com>
2024-11-22 12:14:27 +0100

    Update DivisorsLib.cpp

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -6,6 +6,7 @@ std::vector<unsigned long long> divisors(unsigned long long number)
 	std::vector<unsigned long long> result;
 	for (unsigned long long i = 1; i <= number; ++i)
 	{
+		//faster algorythm
 		if (number % i == 0)
 			result.push_back(i);
 	}
